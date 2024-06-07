e5f4d223f0646b69f5a292100941378bb748813b
Quinn EcSolticia <137521060+EcSolticia@users.noreply.github.com>
2024-06-07 18:27:41 +0600

    Added function creating the desired fourier series (not yet proven)

--- FourierSeries.py
+++ FourierSeries.py
@@ -14,6 +14,8 @@ def delta(index, z):
     return sympy.cos((pie * index)/p * x - (z)*(pie)/2)
 
 def build_list_a():
+    global list_a
+    list_a = [0];
     a_0 = sympy.integrate(f, (x, -p, p)) / (2 * p)
     list_a[0] = a_0;
     
@@ -22,7 +24,18 @@ def build_list_a():
         list_a.append(a_i)
 
 def build_list_b():
-    
+    global list_b
+    list_b = [0];
     for i in range(1, series_size):
         b_i = sympy.integrate(f * delta(i, 1), (x, -p, p)) / p
         list_b.append(b_i)
+
+#returns the fourier function
+def fourier_function():
+    build_list_a(); build_list_b();
+    F = list_a[0]/2
+    for i in range(1, series_size):
+        d = list_a[i] * delta(i, 0) + list_b[i - 1] * delta(i, 1)
+        F = F + d
+    return F
+    
