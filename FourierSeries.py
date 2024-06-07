3b7820072eacd757980593ed9150f45d5395583b
Quinn EcSolticia <137521060+EcSolticia@users.noreply.github.com>
2024-06-07 17:49:36 +0600

    Create FourierSeries.py

---
+++ FourierSeries.py
@@ -0,0 +1,31 @@
+
+import sympy
+
+x = sympy.Symbol('x') #variable
+p = sympy.Symbol('p') #half-period
+pie = sympy.Symbol('pie') #pi symbol
+
+f = x
+
+list_a = [0]; list_b = [0];
+series_size = 10;
+
+def delta(index, z):
+    return sympy.cos((pie * index)/p * x - (z)*(pie)/2)
+
+def build_list_a():
+    a_0 = sympy.integrate(f, (x, -p, p)) / (2 * p)
+    list_a[0] = a_0;
+    
+    for i in range(1, series_size):
+        a_i = sympy.integrate(f * delta(i, 0), (x, -p, p)) / p
+        list_a.append(a_i)
+
+def build_list_b():
+    
+    for i in range(1, series_size):
+        b_i = sympy.integrate(f * delta(i, 1), (x, -p, p)) / p
+        list_b.append(b_i)
+
+build_list_a(); build_list_b();
+print(list_a, list_b);
